1cba4d6e20b85932463e087201c6b85b5a4dcee3
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-09 14:12:12 +0700

    add exercise 02

---
+++ src/test/java/Exercise02Test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class Exercise02Test {
+
+    Exercise02 exercise02 = new Exercise02();
+    @Test
+    public void test_tongCacChuSo() {
+        assertEquals(exercise02.tongCacChuSo(8), 8);
+        assertEquals(exercise02.tongCacChuSo(81), 9);
+        assertEquals(exercise02.tongCacChuSo(8291), 20);
+        assertEquals(exercise02.tongCacChuSo(1091), 11);
+    }
+}
