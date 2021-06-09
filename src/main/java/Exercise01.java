c84415cbc1d967101a4c9c2c0c40f79d86bdeb03
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-09 17:02:43 +0700

    add exercise 01

---
+++ src/main/java/Exercise01.java
@@ -0,0 +1,23 @@
+public class Exercise01 {
+    public int gcd(int n, int m){
+
+        int tmp = 0;
+
+        if(n < m) {
+            n = m + n;
+            m = n - m;
+            n = n - m;
+        }
+        while(m != 0) {
+            tmp = n%m;
+            n = m;
+            m = tmp;
+        }
+
+        return n;
+    }
+
+    public int lcm(int n, int m){
+        return n*m/gcd(n, m);
+    }
+}
---
+++ src/test/java/Exercise01Test.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Exercise01Test {
+    Exercise01 exercise01 = new Exercise01();
+
+    @Test
+    public void test_gcd() {
+        assertEquals(exercise01.gcd(2, 8), 2);
+        assertEquals(exercise01.gcd(12, 8), 4);
+    }
+
+    @Test
+    public void test_lcm() {
+        assertEquals(exercise01.lcm(2, 8), 8);
+        assertEquals(exercise01.lcm(12, 8), 24);
+    }
+}
