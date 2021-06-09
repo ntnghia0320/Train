aa844080ca93097dc6dcd81f9b5e4517bf8cd33c
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-09 21:26:54 +0700

    update master

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,2 +1,25 @@
 public class Demo {
+    public int sum(int x, int y){
+        return x+y;
+    }
+
+    public int sub(int x, int y){
+        return x-y;
+    }
+
+    public int multiple(int x, int y){
+        return x*y;
+    }
+
+    public int max(int x, int y, int z){
+        if(x > y && x > z){
+            return x;
+        }
+
+        if(y > z){
+            return y;
+        }
+
+        return z;
+    }
 }
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,2 +1,33 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DemoTest {
+
+    @Test
+    public void test_sum(){
+        assertEquals(new Demo().sum(2, 8), 10);
+    }
+
+    @Test
+    public void test_sub(){
+        assertEquals(new Demo().sub(8, 3), 5);
+    }
+
+    @Test
+    public void test_multiple(){
+        assertEquals(new Demo().multiple(8, 3), 24);
+    }
+
+    @Test
+    public void test_max(){
+        Demo demo = new Demo();
+        assertEquals(demo.max(8, 3, 7), 8);
+        assertEquals(demo.max(8, 10, 7), 10);
+        assertEquals(demo.max(8, 10, 70), 70);
+        assertEquals(demo.max(8, 5, 70), 70);
+        assertEquals(demo.max(8, 60, 0), 60);
+    }
+
 }
