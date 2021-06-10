04716125ca2871f1dad628f042137a125779a752
TrongNghia <82077885+ntnghia0320@users.noreply.github.com>
2021-06-10 11:13:51 +0700

    Update Exercise06.java

--- src/main/java/Exercise06.java
+++ src/main/java/Exercise06.java
@@ -1,5 +1,5 @@
 public class Exercise06 {
     public int tongDaySo(int n){
-        return n > 0 ? tongDaySo(n-1) + n * (n + 1) : 0;
+        return n > 0 ? tongDaySo(n - 1) + n * (n + 1) : 0;
     }
 }
