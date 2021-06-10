ee6b5300486580846151d92c28f89ef4e5487c53
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-10 20:56:37 +0700

    update Exercise 10

--- src/test/java/Exercise10Test.java
+++ src/test/java/Exercise10Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Exercise10Test {
     Exercise10 exercise10 = new Exercise10();
     @Test
-    public void test_solveQuadraticEquation(){
+    public void test_findDay(){
         assertEquals(exercise10.findDay(5, 8, 2015), "Wednesday");
         assertEquals(exercise10.findDay(10, 6, 2021).toString(), "Thursday");
     }
