34d61351ee19cd9db163bae9ac1ecbab59f6da2b
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 00:10:27 +0700

    Update exercise 08

--- src/main/java/Exercise08.java
+++ src/main/java/Exercise08.java
@@ -1,10 +1,10 @@
 public class Exercise08 {
     public double calculatePi(){
-        final double E = 0.0001;
-        double e = 1, sum = 0.0;
+        double e = 1.0;
+        double sum = 0.0;
         int n = 0;
         
-        while(e > E){
+        while(e > 0.0001){
             if(n % 2 == 0)
                 sum += 1.0 / (2 * n + 1);
             else
@@ -15,6 +15,8 @@ public class Exercise08 {
             n++;
         }
 
-        return sum * 4;
+        sum *= 4;
+
+        return sum;
     }
 }
