5e452ff16a122cf0e6515cf4ed97009c081eb2d9
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-10 20:55:50 +0700

    Add exercise 11

---
+++ src/main/java/Exercise11.java
@@ -0,0 +1,6 @@
+public class Exercise11 {
+    public String findMonth(int month){
+        String[] months = {"None", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        return months[month];
+    }
+}
