b24e54820e4c022b7bfc12a3dc86600bcf772b9a
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 09:12:43 +0700

    Update exercise 10

--- src/main/java/Exercise10.java
+++ src/main/java/Exercise10.java
@@ -3,19 +3,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Exercise10{
-    public static String findDay(int day, int month, int year) {
+    public static String findDay(int day, int month, int year) throws ParseException {
         SimpleDateFormat formatDay = new SimpleDateFormat("yyyy MM dd");
         String input = year + " " + month + " " + day;
         Date date;
 
-        try {
             date = formatDay.parse(input);
             String xDay = String.format("%tA", date);
 
             return xDay;
-        } catch (ParseException e) {
-
-            return "Not Found";
-        }
     }
 }
--- src/test/java/Exercise10Test.java
+++ src/test/java/Exercise10Test.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Exercise10Test {
     Exercise10 exercise10 = new Exercise10();
     @Test
-    public void test_findDay(){
+    public void test_findDay() throws ParseException {
         assertEquals(exercise10.findDay(5, 8, 2015), "Wednesday");
         assertEquals(exercise10.findDay(10, 6, 2021).toString(), "Thursday");
     }
