6a3b62b2c2c745419ef64da37e61d7470b4dfbf0
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 21:21:24 +0700

    Update exercise 08

--- src/main/java/Exercise08.java
+++ src/main/java/Exercise08.java
@@ -1,16 +1,19 @@
 public class Exercise08 {
+    public static final double E = 0.0001;
+    public static final double PI = 3.1415;
+
     public double calculatePi() {
         double e = 1.0;
         double sum = 0.0;
         int n = 0;
 
-        while (e > 0.0001) {
+        while (e > E) {
             if (n % 2 == 0)
                 sum += 1.0 / (2 * n + 1);
             else
                 sum -= 1.0 / (2 * n + 1);
 
-            e = Math.abs(sum * 4 - 3.1415);
+            e = Math.abs(sum * 4 - PI);
 
             n++;
         }
