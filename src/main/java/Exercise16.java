6594a7db525c64391ffa20662731334dc63d6b8c
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 15:12:19 +0700

    Update ex

--- src/main/java/Exercise16.java
+++ src/main/java/Exercise16.java
@@ -1,11 +1,12 @@
 public class Exercise16 {
-    public int countFibonaci(int n){
-        int numberBefore = 1, numberCurrent = 1, countFibonaci = 1;
+    public int countFibonaci(int n) {
+        int numberBefore = 1;
+        int numberCurrent = 1;
+        int countFibonaci = 1;
 
-        while(numberCurrent <= n){
+        while (numberCurrent <= n) {
             numberCurrent = numberBefore + numberCurrent;
             numberBefore = numberCurrent - numberBefore;
-
             countFibonaci++;
         }
 
--- src/test/java/Exercise16Test.java
+++ src/test/java/Exercise16Test.java
@@ -6,7 +6,7 @@ public class Exercise16Test {
     Exercise16 exercise16 = new Exercise16();
 
     @Test
-    public void test_countFibonaci(){
+    public void test_countFibonaci() {
         assertEquals(exercise16.countFibonaci(8), 6);
         assertEquals(exercise16.countFibonaci(7), 5);
         assertEquals(exercise16.countFibonaci(14), 7);
