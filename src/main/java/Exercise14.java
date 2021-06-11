b6d590a92cc69592f9167d5eeb066fcd446a45f8
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 15:01:55 +0700

    Update ex

--- src/main/java/Exercise14.java
+++ src/main/java/Exercise14.java
@@ -1,28 +1,32 @@
 import java.util.ArrayList;
 
 public class Exercise14 {
-    public String findNumber(){
+    public String findNumber() {
         ArrayList<String> listNumberCorrect = new ArrayList<>();
 
-        for(int n = 10; n <= 99999; n++){
-            int sum = 0, tempN = n, powNumber = 0;
+        for (int n = 10; n <= 99999; n++) {
+            int sum = 0;
+            int tempN = n;
+            int powNumber = 0;
             ArrayList<Integer> listNumber = new ArrayList<>();
 
-            while(tempN > 0){
+            while (tempN > 0) {
                 listNumber.add(tempN % 10);
                 powNumber++;
                 tempN /= 10;
             }
 
-            for(Integer i : listNumber){
+            for (Integer i : listNumber) {
                 sum += Math.pow(i, powNumber);
             }
 
-            if(sum == n){
+            if (sum == n) {
                 listNumberCorrect.add(String.valueOf(n));
             }
         }
 
-        return String.join(" ", listNumberCorrect);
+        String result = String.join(" ", listNumberCorrect)
+
+        return result;
     }
 }
