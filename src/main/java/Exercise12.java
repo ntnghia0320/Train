efc21c9a438d37ac739ffeab73103f456f512790
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 09:01:44 +0700

    Update exercise 12

--- src/main/java/Exercise12.java
+++ src/main/java/Exercise12.java
@@ -2,8 +2,10 @@ public class Exercise12 {
     public double sqrt(int a) {
         final double E = 0.0001;
         double fn = a;
+
         while (fn * fn - a > E)
             fn = a / (2 * fn) + fn / 2;
+
         return fn;
     }
 }
