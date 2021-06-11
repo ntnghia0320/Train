9c90bb006839649769cd5e1d1d255d7d8bbe0706
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 11:28:15 +0700

    Add exercise 17

---
+++ src/main/java/Exercise17.java
@@ -0,0 +1,11 @@
+public class Exercise17 {
+    public double calculateSqrt(int n){
+        final double E = 0.001;
+        double numSqrt = 1.0;
+
+        while (Math.abs(numSqrt * numSqrt - n) > E)
+            numSqrt = (n/numSqrt - numSqrt) / 2 + numSqrt;
+
+        return numSqrt;
+    }
+}
