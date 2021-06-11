31c06c02c56108d16d869965a724cf09effc7787
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 15:07:48 +0700

    Update ex

--- src/main/java/Exercise09.java
+++ src/main/java/Exercise09.java
@@ -1,11 +1,11 @@
 public class Exercise09 {
-    public double calculatePi(){
+    public double calculatePi() {
         double e = 1.0;
         double sum = 0.0;
         int n = 2;
 
-        while(e > 0.00001){
-            if(n % 4 == 0)
+        while (e > 0.00001) {
+            if (n % 4 == 0)
                 sum -= 1.0 / (n * (n + 1) * (n + 2));
             else
                 sum += 1.0 / (n * (n + 1) * (n + 2));
