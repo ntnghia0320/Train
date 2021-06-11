bbc3f3f56311abf6e8f89f2c4fdb3eb0c1de0ea5
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-11 15:10:57 +0700

    Update ex

--- src/main/java/Exercise15.java
+++ src/main/java/Exercise15.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 
 public class Exercise15 {
-    public int reverseBit(int n){
+    public int reverseBit(int n) {
         ArrayList<Byte> listBit = new ArrayList<>();
 
-        while(n > 0){
+        while (n > 0) {
             listBit.add((byte) (n % 2));
             n /= 2;
         }
 
-        int powNumber = listBit.size() - 1, result = 0;
+        int powNumber = listBit.size() - 1;
+        int result = 0;
 
-        for(Byte i: listBit){
+        for (Byte i : listBit) {
             result += i * Math.pow(2, powNumber);
             powNumber--;
         }
