5156db4e9a7d011e942cf25a73a4f9e1d8f00dc7
Vo Quang Hoa <voquanghoa@gmail.com>
2021-06-14 17:00:02 +0700

    refactoring

--- src/main/java/Exercise19.java
+++ src/main/java/Exercise19.java
@@ -1,14 +1,18 @@
 public class Exercise19 {
-    public static final String m[] = {"", "M", "MM", "MMM"};
-    public static final String c[] = {"", "C", "CC", "CCC", "CD", "D",
+    public static final String[] m = {
+            "", "M", "MM", "MMM"};
+    public static final String[] c = {
+            "", "C", "CC", "CCC", "CD", "D",
             "DC", "DCC", "DCCC", "CM"};
-    public static final String x[] = {"", "X", "XX", "XXX", "XL", "L",
+    public static final String[] x = {
+            "", "X", "XX", "XXX", "XL", "L",
             "LX", "LXX", "LXXX", "XC"};
-    public static final String i[] = {"", "I", "II", "III", "IV", "V",
+    public static final String[] i = {
+            "",
+            "I", "II", "III", "IV", "V",
             "VI", "VII", "VIII", "IX"};
-    public String intToRoman(int num) {
-        String result = String.join("", m[num / 1000], c[(num % 1000) / 100], x[(num % 100) / 10], i[num % 10]);
 
-        return result;
+    public String intToRoman(int num) {
+        return String.join("", m[num / 1000], c[(num % 1000) / 100], x[(num % 100) / 10], i[num % 10]);
     }
 }
--- src/test/java/Exercise19Test.java
+++ src/test/java/Exercise19Test.java
@@ -7,6 +7,9 @@ public class Exercise19Test {
 
     @Test
     public void test_intToRoman() {
+        assertEquals(exercise19.intToRoman(99), "XCIX");
+        assertEquals(exercise19.intToRoman(499), "CDXCIX");
+        assertEquals(exercise19.intToRoman(9), "IX");
         assertEquals(exercise19.intToRoman(123), "CXXIII");
         assertEquals(exercise19.intToRoman(998), "CMXCVIII");
         assertEquals(exercise19.intToRoman(2948), "MMCMXLVIII");
