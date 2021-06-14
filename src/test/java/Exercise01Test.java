aa478a05f8cd9f5d771293634fef8c7b3c3266d8
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-14 14:48:46 +0700

    Update exercise 01

--- src/test/java/Exercise01Test.java
+++ src/test/java/Exercise01Test.java
@@ -6,7 +6,7 @@ public class Exercise01Test {
     Exercise01 exercise01 = new Exercise01();
 
     @Test
-    public void test_() {
+    public void test_findDoubleNumber() {
         assertEquals(exercise01.findDoubleNumber(new int[]{1, 1, 2, 3}), "1");
         assertEquals(exercise01.findDoubleNumber(new int[]{1, 1, 2, 3, 3, 4, 9, 0, 0, 0}), "1 3");
     }
