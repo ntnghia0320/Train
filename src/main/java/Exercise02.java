d1b8b9e04063d251017b13df0b3fea82e1f16344
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-14 14:47:52 +0700

    Add exercise 02

---
+++ src/main/java/Exercise02.java
@@ -0,0 +1,22 @@
+public class Exercise02 {
+    public int findMaxSum(int[] arrayNumber) {
+        int size = arrayNumber.length;
+        int max = 0;
+        int sum = 0;
+        int indexMax = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < size; j++) {
+                if (arrayNumber[j] >= max) {
+                    max = arrayNumber[j];
+                    indexMax = j;
+                }
+            }
+            sum += max;
+            max = 0;
+            arrayNumber[indexMax] = 0;
+        }
+
+        return sum;
+    }
+}
