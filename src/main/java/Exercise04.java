ebb967c0bcc2fff43ea17d51083ed593de69697b
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-15 10:38:56 +0700

    Update exercise 04

--- src/main/java/Exercise04.java
+++ src/main/java/Exercise04.java
@@ -1,64 +1,31 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Exercise04 {
-    public String sortEvenOdd(Integer[] arrayNumber) {
-        ArrayList<Integer> arrEven = new ArrayList<>();
-        ArrayList<Integer> arrOdd = new ArrayList<>();
-        ArrayList<String> listNumberResult = new ArrayList<>();
+    public Integer[] sortEvenOdd(Integer[] arr) {
+        int n = arr.length;
+        int l = 0;
+        int r = n - 1;
 
-        for (Integer i : arrayNumber) {
-            if (i % 2 == 0) {
-                arrEven.add(i);
-            } else {
-                arrOdd.add(i);
+        while (l < r) {
+            while (arr[l] % 2 == 0 && l < r) l++;
+
+            while (arr[r] % 2 != 0 && l < r) r--;
+
+            if (l < r) {
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
             }
+            System.out.print(l + " " + r);
+            System.out.print("\n");
         }
 
-        sort(arrEven, 0, arrEven.size() - 1);
-        sort(arrOdd, 0, arrOdd.size() - 1);
+        if (l == n - 1) l = n;
 
-        for (Integer i : arrEven) {
-            listNumberResult.add(String.valueOf(i));
-        }
+        Arrays.sort(arr, 0, l);
+        Arrays.sort(arr, l, n, Collections.reverseOrder());
 
-        int size = arrOdd.size() - 1;
-
-        for (int i = size; i >= 0; i--) {
-            listNumberResult.add(String.valueOf(arrOdd.get(i)));
-        }
-
-        String result = String.join(" ", listNumberResult);
-
-        return result;
+        return arr;
     }
-
-    int partition(ArrayList<Integer> arr, int low, int high) {
-        int pivot = arr.get(high);
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (arr.get(j) < pivot) {
-                i++;
-
-                int temp = arr.get(i);
-                arr.set(i, arr.get(j));
-                arr.set(j, temp);
-            }
-        }
-
-        int temp = arr.get(i + 1);
-        arr.set(i + 1, arr.get(high));
-        arr.set(high, temp);
-
-        return i + 1;
-    }
-
-    void sort(ArrayList<Integer> arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-
-            sort(arr, low, pi - 1);
-            sort(arr, pi + 1, high);
-        }
-    }
-
 }
--- src/test/java/Exercise04Test.java
+++ src/test/java/Exercise04Test.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Exercise04Test {
@@ -7,8 +8,11 @@ public class Exercise04Test {
 
     @Test
     public void test_sortEvenOdd() {
-        assertEquals(exercise04.sortEvenOdd(new Integer[]{1, 2, 3, 4, 5, 6, 8, 3, 5, 6, 23, 34, 32}), "2 4 6 6 8 32 34 23 5 5 3 3 1");
-        assertEquals(exercise04.sortEvenOdd(new Integer[]{1, 3, 5, 7, 5, 7, 5, 9, 21, 43}), "43 21 9 7 7 5 5 5 3 1");
-        assertEquals(exercise04.sortEvenOdd(new Integer[]{2, 4, 2, 4, 42, 6, 8, 64, 52, 54}), "2 2 4 4 6 8 42 52 54 64");
+        assertArrayEquals(exercise04.sortEvenOdd(new Integer[]{1, 2, 3, 4, 5, 6, 8, 3, 5, 6, 23, 34, 32}),
+                new Integer[]{2, 4, 6, 6, 8, 32, 34, 23, 5, 5, 3, 3, 1});
+        assertArrayEquals(exercise04.sortEvenOdd(new Integer[]{1, 3, 5, 7, 5, 7, 5, 9, 21, 43}),
+                new Integer[]{43, 21, 9, 7, 7, 5, 5, 5, 3, 1});
+        assertArrayEquals(exercise04.sortEvenOdd(new Integer[]{2, 4, 2, 4, 42, 6, 8, 64, 52, 54}),
+                new Integer[]{2, 2, 4, 4, 6, 8, 42, 52, 54, 64});
     }
 }
