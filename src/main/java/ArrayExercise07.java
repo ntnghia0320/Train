b8bd01f82ff9ebae162bdcbacc32b09ba73e9abf
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-15 14:19:45 +0700

    Add exercise 07

---
+++ src/main/java/ArrayExercise07.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ArrayExercise07 {
+    public int[] findSubArray(int[] arr) {
+        int n = arr.length - 1;
+        int lenMax = 1;
+        int lenCurrent = 1;
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i + 1]) {
+                lenCurrent++;
+            } else {
+                if (lenMax < lenCurrent) {
+                    lenMax = lenCurrent;
+                    maxIndex = i + 1;
+                }
+                lenCurrent = 1;
+            }
+        }
+
+        if (lenMax < lenCurrent) {
+            lenMax = lenCurrent;
+            maxIndex = n + 1;
+        }
+
+        int indexStart = maxIndex - lenMax;
+
+        return Arrays.copyOfRange(arr, indexStart, maxIndex);
+    }
+}
