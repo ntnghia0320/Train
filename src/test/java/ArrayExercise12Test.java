edabef78100957b7e6e25816dbb0a1b6b2af72cc
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-15 23:36:59 +0700

    Update exercise

--- src/test/java/ArrayExercise12Test.java
+++ src/test/java/ArrayExercise12Test.java
@@ -7,7 +7,7 @@ public class ArrayExercise12Test {
     ArrayExercise12 arrayExercise12 = new ArrayExercise12();
 
     @Test
-    public void test_() {
+    public void test_checkSimilarArrays() {
         assertTrue(arrayExercise12.checkSimilarArrays(new int[]{1, 3, 4, 2}, new int[]{2, 3, 4, 1}));
         assertFalse(arrayExercise12.checkSimilarArrays(new int[]{1, 3, 5, 2}, new int[]{2, 3, 4, 1}));
         assertTrue(arrayExercise12.checkSimilarArrays(new int[]{1, 3, 4, 2, 0, 0, 1, 2}, new int[]{2, 3, 0, 2, 1, 0, 4, 1}));
