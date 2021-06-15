8e478d52209bdc1463f6409bb860c0210148bc1a
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-15 08:43:10 +0700

    UpdateEx09

--- src/main/java/Exercise09.java
+++ src/main/java/Exercise09.java
@@ -1,23 +1,16 @@
 public class Exercise09 {
     public static final double E = 0.00001;
-    public static final double PI = 3.14159;
 
     public double calculatePi() {
         double e = 1.0;
-        double sum = 0.0;
+        double sum = 3.0;
         int n = 2;
 
         while (e > E) {
-            if (n % 4 == 0)
-                sum -= 1.0 / (n * (n + 1) * (n + 2));
-            else
-                sum += 1.0 / (n * (n + 1) * (n + 2));
-
-            e = Math.abs(3 + sum * 4 - PI);
+            e = 4.0 / (n * (n + 1) * (n + 2));
+            sum += n % 4 == 0 ? -e : e;
             n += 2;
         }
-
-        sum = 3.14159 + e;
 
         return sum;
     }
--- src/test/java/Exercise09Test.java
+++ src/test/java/Exercise09Test.java
@@ -7,6 +7,6 @@ public class Exercise09Test {
 
     @Test
     public void test_calculatePi() {
-        assertEquals(exercise09.calculatePi(), 3.1415975817522517);
+        assertEquals(exercise09.calculatePi(), 3.14159, exercise09.E);
     }
 }
