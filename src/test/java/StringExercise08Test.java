2cb3e60ddf00f82820b788624de965cde9dea84d
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-16 16:01:00 +0700

    Update exercise 08

--- src/test/java/StringExercise08Test.java
+++ src/test/java/StringExercise08Test.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringExercise08Test {
@@ -8,5 +9,6 @@ public class StringExercise08Test {
     @Test
     public void test(){
         assertTrue(stringExercise08.checkBarcode("8938505974194"));
+        assertFalse(stringExercise08.checkBarcode("8938505974194"));
     }
 }
