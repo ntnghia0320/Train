d567ad7055e5b030470d4a81d25ca6f99c1b8de1
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-16 11:51:25 +0700

    Add exercise 04

---
+++ src/main/java/StringExercise04.java
@@ -0,0 +1,34 @@
+public class StringExercise04 {
+    static void computeLPSArray(String str, int M, int[] lps) {
+        int len = 0;
+        int i;
+
+        lps[0] = 0;
+        i = 1;
+
+        while (i < M) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    public boolean isRepeatString(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+
+        computeLPSArray(str, n, lps);
+
+        int len = lps[n - 1];
+        return len > 0 && n % (n - len) == 0;
+    }
+}
---
+++ src/test/java/StringExercise04Test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringExercise04Test {
+    StringExercise04 stringExercise04 = new StringExercise04();
+
+    @Test
+    public void test_(){
+        assertTrue(stringExercise04.isRepeatString("abcabc"));
+        assertTrue(stringExercise04.isRepeatString("abcabcabc"));
+        assertFalse(stringExercise04.isRepeatString("abcabcbca"));
+    }
+}
