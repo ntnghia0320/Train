e07ed06950c6ec40f2beb1c8e7032e0d1cef4eea
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-16 14:01:18 +0700

    Update exercise 05

--- src/main/java/StringExercise05.java
+++ src/main/java/StringExercise05.java
@@ -5,7 +5,7 @@ public class StringExercise05 {
     public String findRepeatChar(String str){
         int n  = str.length();
         HashMap<Character, Integer> hashMap = new HashMap<>();
-        ArrayList<Character> result = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
 
         for(int i = 0;i<n;i++){
             hashMap.merge(str.charAt(i), 1, Integer::sum);
@@ -13,10 +13,10 @@ public class StringExercise05 {
 
         for (Character i : hashMap.keySet()) {
             if (hashMap.get(i) >= 2) {
-                result.add(i);
+                result.append(i);
             }
         }
 
-        return result.toString().replaceAll("[,\\s\\p{Blank}\\[\\]]", "");
+        return result.toString();
     }
 }
