8dd76e40c303db96046f96b3d536e3c5d10cd5b7
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-16 22:13:57 +0700

    Update exercise

--- src/main/java/ArrayExercise06.java
+++ src/main/java/ArrayExercise06.java
@@ -1,33 +1,14 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class ArrayExercise06 {
     public int[] sort(int[] arr) {
-        ArrayList<Integer> listEven = new ArrayList<>();
-        ArrayList<Integer> listOdd = new ArrayList<>();
-
-        for (int i : arr) {
-            if (i % 2 == 0) {
-                listEven.add(i);
-            } else {
-                listOdd.add(i);
-            }
-        }
-
-        Collections.sort(listEven);
-        listOdd.sort(Collections.reverseOrder());
-
-        int evenIndex = 0;
-        int oddIndex = 0;
-        int size = arr.length;
-
-        for (int i = 0; i < size; i++) {
-            if (arr[i] % 2 == 0) {
-                arr[i] = listEven.get(evenIndex);
-                evenIndex++;
-            } else {
-                arr[i] = listOdd.get(oddIndex);
-                oddIndex++;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] % 2 == arr[j] % 2) {
+                    if ((arr[i] > arr[j]) == (arr[i] % 2 == 0)) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
             }
         }
 
--- src/test/java/TestArrayExercise06.java
+++ src/test/java/TestArrayExercise06.java
@@ -6,7 +6,7 @@ public class TestArrayExercise06 {
     ArrayExercise06 arrayExercise06 = new ArrayExercise06();
 
     @Test
-    public void test_(){
+    public void test_sort() {
         assertArrayEquals(arrayExercise06.sort(new int[]{3, 12, 10, 41, -2, 7}), new int[]{41, -2, 10, 7, 12, 3});
         assertArrayEquals(arrayExercise06.sort(new int[]{1, 3, 5, -1, 6, 4}), new int[]{5, 3, 1, -1, 4, 6});
     }
