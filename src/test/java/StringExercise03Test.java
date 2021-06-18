50612be51d65aacb62ab2ffeefa0a7328b4afc48
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-18 22:30:41 +0700

    Update exercise

--- src/test/java/StringExercise03Test.java
+++ src/test/java/StringExercise03Test.java
@@ -6,8 +6,8 @@ public class StringExercise03Test {
     StringExercise03 stringExercise03 = new StringExercise03();
 
     @Test
-    public void test_(){
-        assertEquals(stringExercise03.findSumNumber("abc 123 def 33 mn 3.221"),380);
-        assertEquals(stringExercise03.findSumNumber("ab100 def0mn10.2000.... 0 0 ,"),2110);
+    public void test_findSumNumber() {
+        assertEquals(stringExercise03.findSumNumber("abc 123 def 33 mn 3.221"), 380);
+        assertEquals(stringExercise03.findSumNumber("ab100 def0mn10.2000.... 0 0 ,"), 2110);
     }
 }
