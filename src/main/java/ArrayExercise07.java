d22ba4b9ba2b94bdb5803c4b67956770f98ffa52
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-18 11:31:36 +0700

    Update exercise

--- src/main/java/ArrayExercise07.java
+++ src/main/java/ArrayExercise07.java
@@ -2,30 +2,22 @@ import java.util.Arrays;
 
 public class ArrayExercise07 {
     public int[] findSubArray(int[] arr) {
-        int n = arr.length - 1;
         int lenMax = 1;
         int lenCurrent = 1;
         int maxIndex = 1;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] < arr[i + 1]) {
                 lenCurrent++;
-            } else {
                 if (lenMax < lenCurrent) {
                     lenMax = lenCurrent;
-                    maxIndex = i + 1;
+                    maxIndex = i + 2;
                 }
+            }else {
                 lenCurrent = 1;
             }
         }
 
-        if (lenMax < lenCurrent) {
-            lenMax = lenCurrent;
-            maxIndex = n + 1;
-        }
-
-        int indexStart = maxIndex - lenMax;
-
-        return Arrays.copyOfRange(arr, indexStart, maxIndex);
+        return Arrays.copyOfRange(arr, maxIndex - lenMax, maxIndex);
     }
 }
