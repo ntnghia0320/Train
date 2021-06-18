a46a62dcfaef2e1f2fca66a6ccb3bb50c6c10d76
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-19 00:09:20 +0700

    Update exercise

--- src/main/java/StringExercise06.java
+++ src/main/java/StringExercise06.java
@@ -1,26 +1,19 @@
 public class StringExercise06 {
     public String shortcutString(String str) {
-        int n = str.length() - 1;
+        str+=' ';
         int count = 1;
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == str.charAt(i + 1)) {
                 count++;
-            } else if (count > 1) {
-                result.append(str.charAt(i));
-                result.append(count);
-                count = 1;
             } else {
                 result.append(str.charAt(i));
+                if (count > 1) {
+                    result.append(count);
+                    count = 1;
+                }
             }
-        }
-
-        if (count > 1) {
-            result.append(str.charAt(n - 1));
-            result.append(count);
-        } else {
-            result.append(str.charAt(n));
         }
 
         return String.valueOf(result);
