b965e84a9999b07ee20069e8e0b37d122205261f
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-18 22:36:55 +0700

    Update exercise

--- src/test/java/StringExercise04Test.java
+++ src/test/java/StringExercise04Test.java
@@ -7,7 +7,7 @@ public class StringExercise04Test {
     StringExercise04 stringExercise04 = new StringExercise04();
 
     @Test
-    public void test_(){
+    public void test_isRepeatString() {
         assertTrue(stringExercise04.isRepeatString("abcabc"));
         assertTrue(stringExercise04.isRepeatString("abcabcabc"));
         assertFalse(stringExercise04.isRepeatString("abcabcbca"));
