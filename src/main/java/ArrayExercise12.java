e277fc179b8870450c42a4edac493725e07b0111
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-18 21:32:26 +0700

    Update exercise

--- src/main/java/ArrayExercise12.java
+++ src/main/java/ArrayExercise12.java
@@ -1,14 +1,19 @@
-import java.util.Arrays;
-
 public class ArrayExercise12 {
     public boolean checkSimilarArrays(int[] a, int[] b) {
-        Arrays.sort(a);
-        Arrays.sort(b);
+        int count = 0;
 
-        for (int i = 0; i < a.length ; i++) {
-            if (a[i] != b[i]) return false;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    count++;
+                    int temp = b[i];
+                    b[i] = b[j];
+                    b[j] = temp;
+                    break;
+                }
+            }
         }
 
-        return true;
+        return count == a.length;
     }
 }
