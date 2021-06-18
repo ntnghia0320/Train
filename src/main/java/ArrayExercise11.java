8cfa8da210138fb41a9c1766a3d2b8647dc41cdc
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-18 21:21:10 +0700

    Update exercise

--- src/main/java/ArrayExercise11.java
+++ src/main/java/ArrayExercise11.java
@@ -1,11 +1,10 @@
 public class ArrayExercise11 {
     public int maxSumSubArray(int[] arr) {
-        int size = arr.length;
         int max = arr[0];
         int maxCurrent = 0;
 
-        for (int i = 0; i < size; i++) {
-            maxCurrent += arr[i];
+        for (int i : arr) {
+            maxCurrent += i;
             if (max < maxCurrent) max = maxCurrent;
             if (maxCurrent < 0) maxCurrent = 0;
         }
