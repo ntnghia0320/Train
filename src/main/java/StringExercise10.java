cfe8511a17617f9d2976ecabeb5e2b0f71c84d11
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-22 15:46:59 +0700

    Add Exercise 10

--- src/main/java/StringExercise10.java
+++ src/main/java/StringExercise10.java
@@ -1,33 +1,35 @@
 public class StringExercise10 {
-    public String subStringNumber(String strNum1, String strNum2) {
+    int digitFromRight(String str, int position) {
+        if (position >= str.length()) return 0;
+        return str.charAt(str.length() - 1 - position) - '0';
+    }
+
+    public int subStringNumber(String strNum1, String strNum2) {
         if (strNum1.length() < strNum2.length()) {
             return subStringNumber(strNum2, strNum1);
         }
 
-        int surplus = 0;
-        int k = strNum1.length() - strNum2.length();
-        StringBuilder result = new StringBuilder();
+        int sign1 = 1;
+        int sign2 = 1;
+        int level = 1;
+        int sum = 0;
 
-        for (int i = strNum1.length() - 1; i >= 0; i--) {
-            int sum = i >= k ? strNum2.charAt(i - k) - '0' + strNum1.charAt(i) - '0' : strNum1.charAt(i) - '0';
-
-            if (surplus > 0) {
-                sum += surplus;
-                surplus = 0;
-            }
-
-            if (sum > 9) {
-                surplus = sum / 10;
-                sum = sum % 10;
-            }
-
-            result.append(sum);
+        if (strNum1.charAt(0) == '-') {
+            strNum1 = strNum1.substring(1);
+            sign1 = -1;
         }
 
-        if (surplus > 0) {
-            result.append(surplus);
+        if (strNum2.charAt(0) == '-') {
+            strNum2 = strNum2.substring(1);
+            sign2 = -1;
         }
 
-        return result.reverse().toString();
+        for (int i = 0; i < strNum1.length(); i++) {
+            sum += (sign1 * digitFromRight(strNum1, i) + sign2 * digitFromRight(strNum2, i)) * level;
+            System.out.println(strNum1.length());
+            level *= 10;
+        }
+
+        return sum;
     }
 }
--- src/test/java/StringExercise10Test.java
+++ src/test/java/StringExercise10Test.java
@@ -6,9 +6,12 @@ public class StringExercise10Test {
     StringExercise10 stringExercise10 = new StringExercise10();
 
     @Test
-    public void test_subStringNumber(){
-        assertEquals(stringExercise10.subStringNumber("789", "456"), "1245");
-        assertEquals(stringExercise10.subStringNumber("2789", "456"), "3245");
-        assertEquals(stringExercise10.subStringNumber("456", "2789"), "3245");
+    public void test_subStringNumber() {
+        assertEquals(stringExercise10.subStringNumber("789", "456"), 1245);
+        assertEquals(stringExercise10.subStringNumber("789", "-456"), 333);
+        assertEquals(stringExercise10.subStringNumber("7", "-15"), -8);
+        assertEquals(stringExercise10.subStringNumber("1", "-9"), -8);
+        assertEquals(stringExercise10.subStringNumber("2789", "456"), 3245);
+        assertEquals(stringExercise10.subStringNumber("456", "2789"), 3245);
     }
 }
