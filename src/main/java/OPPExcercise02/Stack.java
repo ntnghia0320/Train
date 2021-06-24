30eeb81a36a474956bf5640132fd4b6ff8ae34ad
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-24 16:47:55 +0700

    Update exercise

--- src/main/java/OPPExcercise02/Stack.java
+++ src/main/java/OPPExcercise02/Stack.java
@@ -1,4 +1,39 @@
 package OPPExcercise02;
 
 public class Stack {
+    int top;
+    int[] a = new int[1000];
+
+    public Stack() {
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return (top < 0);
+    }
+
+    public boolean push(int x) {
+        if (top >= (1000 - 1)) {
+            return false;
+        } else {
+            a[++top] = x;
+            return true;
+        }
+    }
+
+    public int pop() {
+        if (top < 0) {
+            return 0;
+        } else {
+            return a[top--];
+        }
+    }
+
+    public int peek() {
+        if (top < 0) {
+            return 0;
+        } else {
+            return a[top];
+        }
+    }
 }
---
+++ src/test/java/OOPExercise02Test.java
@@ -0,0 +1,25 @@
+import OPPExcercise02.Stack;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class OOPExercise02Test {
+    Stack stack = new Stack();
+
+    @Test
+    void test_isEmpty() {
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    void test_push() {
+    }
+
+    @Test
+    void test_pop() {
+    }
+
+    @Test
+    void test_peek() {
+    }
+}
