5d36f40b034f3a073aacebaac17d600df349283b
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-24 13:40:16 +0700

    Update exercise

--- src/main/java/OOPExercise01/Student.java
+++ src/main/java/OOPExercise01/Student.java
@@ -1,12 +1,12 @@
 package OOPExercise01;
 
 public class Student {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String birthday;
-    private String className;
-    private String address;
+    private final int id;
+    private final String firstName;
+    private final String lastName;
+    private final String birthday;
+    private final String className;
+    private final String address;
 
     public Student(int id, String firstName, String lastName, String birthday, String className, String address) {
         this.id = id;
@@ -15,6 +15,73 @@ public class Student {
         this.birthday = birthday;
         this.className = className;
         this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public static class Builder {
+        private int id;
+        private String firstName;
+        private String lastName;
+        private String birthday;
+        private String className;
+        private String address;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setBirthday(String birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public Builder setClassName(String className) {
+            this.className = className;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Student build() {
+            return new Student(id, firstName, lastName, birthday, className, address);
+        }
     }
 
     @Override
--- src/main/java/OOPExercise01/StudentController.java
+++ src/main/java/OOPExercise01/StudentController.java
@@ -1,43 +1,73 @@
 package OOPExercise01;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class StudentController {
-    List<Student> students = new ArrayList<Student>();
+    List<Student> students = new ArrayList<>();
 
-    public void addStudent() {
-        StudentBuilder student = new StudentBuilder().setId(1);
-        students.add(student.build());
-        System.out.println(students.get(0).toString());
+    public void add(Student student) {
+        students.add(student);
     }
 
-    public void deleteStudent() {
-
+    public void delete(Student student) {
+        students.remove(student);
     }
 
-    public void saveToFile(String path) {
-
+    public void saveToFile(String path) throws IOException {
+        String json = new Gson().toJson(students);
+        FileWriter myWriter = new FileWriter(path + "student.txt");
+        myWriter.write(json);
+        myWriter.close();
     }
 
-    public void loadFromFile(String path) {
+    public void loadFromFile(String path) throws FileNotFoundException {
+        File file = new File(path + "student.txt");
+        Scanner scan = new Scanner(file);
+        scan.useDelimiter("\\Z");
+        String json = scan.next();
+        List<Student> students = new Gson().fromJson(json, new TypeToken<List<Student>>() {
+        }.getType());
+        scan.close();
 
+        this.students.clear();
+        this.students.addAll(students);
     }
 
     public Student getById(int id) {
+        for (Student student : students) {
+            if (student.getId() == id) return student;
+        }
+
         return null;
     }
 
-    public Student[] findByName(String name) {
-        return null;
+    public List<Student> findByName(String name) {
+        List<Student> listStudentFoundByName = new ArrayList<>();
+
+        for (Student student : students) {
+            if ((student.getFirstName() + student.getLastName()).equals(name)) listStudentFoundByName.add(student);
+        }
+
+        return listStudentFoundByName;
     }
 
-    public Student[] findByClassName(String className) {
-        return null;
-    }
-    public static void main(String[] args) {
-        StudentController st = new StudentController();
-        st.addStudent();
+    public List<Student> findByClassName(String className) {
+        List<Student> listStudentFoundByClassName = new ArrayList<>();
+
+        for (Student student : students) {
+            if ((student.getClassName()).equals(className)) listStudentFoundByClassName.add(student);
+        }
+
+        return listStudentFoundByClassName;
     }
 }
 
