bd3f18481cc6f7588b09fddb60388ee290e41999
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-24 14:29:59 +0700

    Update exercise

---
+++ src/test/java/OOPExercise01/StudentControllerTest.java
@@ -0,0 +1,34 @@
+package OOPExercise01;
+
+import org.junit.jupiter.api.Test;
+
+class StudentControllerTest {
+
+    @Test
+    public void test_add() {
+    }
+
+    @Test
+    public void test_delete() {
+    }
+
+    @Test
+    public void test_saveToFile() {
+    }
+
+    @Test
+    public void test_loadFromFile() {
+    }
+
+    @Test
+    public void test_getById() {
+    }
+
+    @Test
+    public void test_findByName() {
+    }
+
+    @Test
+    public void test_findByClassName() {
+    }
+}
