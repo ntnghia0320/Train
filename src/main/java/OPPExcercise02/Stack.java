bdc1cb2c93e7e9e7dd23f8373b939190a484789b
Nghia Nguyen <ntnghia0320@gmail.com>
2021-06-25 09:54:47 +0700

    Update exercise

--- src/main/java/OPPExcercise02/Stack.java
+++ src/main/java/OPPExcercise02/Stack.java
@@ -1,39 +1,28 @@
 package OPPExcercise02;
 
 public class Stack {
-    int top;
-    int[] a = new int[1000];
-
-    public Stack() {
-        top = -1;
-    }
+    private final int[] elements = new int[1000];
+    private int top = -1;
 
     public boolean isEmpty() {
-        return (top < 0);
+        return top < 0;
     }
 
-    public boolean push(int x) {
-        if (top >= (1000 - 1)) {
-            return false;
-        } else {
-            a[++top] = x;
-            return true;
-        }
+    public void push(int element) {
+            elements[++top] = element;
     }
 
     public int pop() {
-        if (top < 0) {
-            return 0;
-        } else {
-            return a[top--];
+        if (top >= 0) {
+            return elements[top--];
         }
+        throw new RuntimeException("Stack Underflow");
     }
 
     public int peek() {
-        if (top < 0) {
-            return 0;
-        } else {
-            return a[top];
+        if (top >= 0) {
+            return elements[top];
         }
+        throw new RuntimeException("Stack Underflow");
     }
 }
--- src/test/java/OOPExercise02Test.java
+++ src/test/java/OOPExercise02Test.java
@@ -1,25 +1,60 @@
 import OPPExcercise02.Stack;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OOPExercise02Test {
-    Stack stack = new Stack();
+    Stack stack;
 
-    @Test
-    void test_isEmpty() {
-        assertTrue(stack.isEmpty());
-    }
+    @Nested
+    class WhenNew {
 
-    @Test
-    void test_push() {
-    }
+        @BeforeEach
+        void innit() {
+            stack = new Stack();
+        }
 
-    @Test
-    void test_pop() {
-    }
+        @Test
+        void test_isEmpty() {
+            assertTrue(stack.isEmpty());
+        }
 
-    @Test
-    void test_peek() {
+        @Test
+        void test_pop() {
+            assertThrows(RuntimeException.class, stack::pop);
+        }
+
+        @Test
+        void test_peek() {
+            assertThrows(RuntimeException.class, stack::peek);
+        }
+
+        @Nested
+        class AfterPushing {
+
+            @BeforeEach
+            void test_push() {
+                stack.push(1);
+            }
+
+            @Test
+            void test_isEmpty() {
+                assertFalse(stack.isEmpty());
+            }
+
+            @Test
+            void test_pop() {
+                assertEquals(1, stack.pop());
+                assertTrue(stack.isEmpty());
+            }
+
+            @Test
+            void test_peek() {
+                assertEquals(1, stack.peek());
+                assertFalse(stack.isEmpty());
+            }
+        }
     }
 }
